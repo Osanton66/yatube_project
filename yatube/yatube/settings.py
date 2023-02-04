2b4f83e993d601a2c288ac566c4f613d95d37ca4
anton <osanton@mail.ru>
2023-02-05 00:22:34 +0300

    password reset and change added

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
     'users.apps.UsersConfig',
     'posts.apps.PostsConfig',
     'django.contrib.admin',
@@ -111,3 +112,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+#LOGOUT_REDIRECT_URL = 'posts:index' 
+#PASSWORD_CHANGE = 'users:password_change_done'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
